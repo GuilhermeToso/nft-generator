f274761c0c56483a23a7b8b91dca24e36727eddb
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 20:03:35 -0300

    Created Contact page component

--- src/Views/Contact/index.jsx
+++ src/Views/Contact/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Body } from "../../Components/Body";
 import { NonEditorStyle } from "../../Styles";
+import { Header } from "../../Components/Header";
 const Contact = () => {
     return (
         <Body
             Background={NonEditorStyle.Background}
             HeaderHeight={NonEditorStyle.HeaderHeight}
         >
+            <Header displayNav={true}></Header>
             <h1>Contact Page</h1>
         </Body>
     )
