ffe6638d6426390c200fa787908452073d997ca6
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 20:02:44 -0300

    Created style objects

--- src/Styles/index.js
+++ src/Styles/index.js
@@ -2,3 +2,23 @@ export const NonEditorStyle = {
     Background: true,
     HeaderHeight: 80
 }
+
+export const EditorStyle = {
+    Background: false,
+    HeaderHeight: 80
+}
+
+export const HomeMainStyle = {
+    GridTemplateRows:"1fr 3fr",
+    GridTemplateColumns:"1fr",
+    GridTemplateAreas: `
+        'title'
+        'button'
+    `,
+    ButtonWidth: "15",
+    ButtonHeight:"10",
+    ButtonColor:"#4370F5",
+    ButtonMinWidth:"200",
+    TitlePadTop: "30",
+    TitleWidth:"50"
+}
