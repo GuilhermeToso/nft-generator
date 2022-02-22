ba6ed8be10910630133930fb1c606feb5a416dc8
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 20:06:58 -0300

    Created GlobalStyle component

---
+++ src/index.styles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    body {
+        width: 100%;
+        height: 100vh;
+    }
+
+    #root {
+        width: 100%;
+        height: 100vh;
+    }
+`
+
+export {GlobalStyle}
