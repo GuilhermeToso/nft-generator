6974b614385a52ea70c9a1e8440d8f9484ddd41a
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 20:03:04 -0300

    Created About page component

--- src/Views/About/index.jsx
+++ src/Views/About/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Body } from "../../Components/Body";
 import { NonEditorStyle } from "../../Styles";
-
+import { Header } from "../../Components/Header";
 const About = () => {
     return (
         <Body
             Background={NonEditorStyle.Background}
             HeaderHeight={NonEditorStyle.HeaderHeight}
         >
+            <Header displayNav={true}></Header>
             <h1>About Page</h1>
         </Body>
     )
