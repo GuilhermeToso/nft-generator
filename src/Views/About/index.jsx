bdffe21ca312555b44a7e8c197cece7aa0998153
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 14:05:29 -0300

    Update Body Attributes

--- src/Views/About/index.jsx
+++ src/Views/About/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Body } from "../../Components/Body";
+import { NonEditorStyle } from "../../Styles";
 
 const About = () => {
     return (
-        <Body>
+        <Body
+            Background={NonEditorStyle.Background}
+            HeaderHeight={NonEditorStyle.HeaderHeight}
+        >
             <h1>About Page</h1>
         </Body>
     )
