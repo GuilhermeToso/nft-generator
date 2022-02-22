da871ac0e22bd6259e2d917fa84c48f45dde32d8
Guilherme M. Toso <guilhermetoso@hotmail.com>
2022-02-22 14:05:53 -0300

    Update Body Attributes

--- src/Views/Contact/index.jsx
+++ src/Views/Contact/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Body } from "../../Components/Body";
-
+import { NonEditorStyle } from "../../Styles";
 const Contact = () => {
     return (
-        <Body>
+        <Body
+            Background={NonEditorStyle.Background}
+            HeaderHeight={NonEditorStyle.HeaderHeight}
+        >
             <h1>Contact Page</h1>
         </Body>
     )
